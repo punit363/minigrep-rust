4f17be9150402cb6917905f1f4249258aaa853de
Punit <3033.punit@gmail.com>
2026-08-24 15:52:07 +0530

    Seperated logic to functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
+use core::error;
 use std::env;
+use std::fs;
+use std::process;
+use crate::error::Error;
 
 fn main() {
     //.args() an iterator over the arguments passed in terminal
     //.collect() packs the input in vector
-    let args : Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("query: {}, filename: {}",query, filename);
-    println!("args: {:?}",args);
+    let config = Config::new(&args).unwrap_or_else(|error| {
+        println!("Error: {}", error);
+        process::exit(1)
+    });
+    println!("query: {}, filename: {}", config.query, config.filename);
+    println!("args: {:?}", args);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.filename)?;
+
+    println!("With text: \n{}", content);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
